7443834ac187b337a3ea3bda16abf2622fef49e0
Muhammad Arham <iqbalarham43@gmail.com>
2023-05-20 14:44:54 +0500

    DEBUGGED SERVER PROXY FAILURE

--- client/src/store/actions/products.js
+++ client/src/store/actions/products.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { FETCH_PRODUCTS } from './types';
 
-export const fetchProducts = () => async dispatch => {
-  try {
-    const res = await axios("/api/products/")
-    dispatch({ 
-       type: FETCH_PRODUCTS,
-       payload: res.data.products
-    })
-  } catch (err) {
-    console.log(err.message);
-  }  
+export const fetchProducts = () => async (dispatch) => {
+	try {
+		const res = await axios('/api/products/');
+		dispatch({
+			type: FETCH_PRODUCTS,
+			payload: res.data.products,
+		});
+	} catch (err) {
+		console.log(err.message);
+	}
 };
